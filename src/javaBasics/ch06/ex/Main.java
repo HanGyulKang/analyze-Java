445ca6c3f9ad40ec96f96ac28ea8f3129de2a2d9
HanGyulKang <gang6607@gmail.com>
2023-09-27 00:08:21 +0900

    method

--- src/javaBasics/ch06/ex/Main.java
+++ src/javaBasics/ch06/ex/Main.java
@@ -1,6 +1,13 @@
 package javaBasics.ch06.ex;
 
+import java.util.Arrays;
+
 public class Main {
+
+    private static void printArrayContents(int[] numArr) {
+        Arrays.stream(numArr)
+                .forEach(num -> System.out.printf("%d", num));
+    }
 
     public static class Tv {
         public Tv() {
@@ -18,5 +25,15 @@ public class Main {
         System.out.println(tvArr[0].hashCode());
         System.out.println(tvArr[1].hashCode());
         System.out.println(tvArr[2].hashCode());
+
+        int[] numArr = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            numArr[i] = (int) (Math.random() * 10);
+        }
+
+        // 동일한 코드를 메서드로 묶어서 코드를 간결하게 만듦 : 캡슐화의 시작...
+        printArrayContents(numArr);
+        printArrayContents(numArr);
     }
 }
