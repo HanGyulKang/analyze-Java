35289929c5abc7c3c22cb49f85ff661f091c743b
HanGyulKang <gang6607@gmail.com>
2023-10-03 23:01:53 +0900

    parameters

---
+++ src/javaBasics/ch02/ex/Main.java
@@ -0,0 +1,31 @@
+package javaBasics.ch02.ex;
+
+class Data {
+    int x;
+}
+
+class Main {
+    public static void main(String[] args) {
+        Data d = new Data();
+        d.x = 10;
+        System.out.println("main() : x = " + d.x);
+
+        change(d.x);
+        System.out.println("After change(d.x)");
+        // 참조변수 d의 x값을 바꾼 것이 아니라 change method 안의 지역변수 x의 값만 바꿨기 때문에
+        // 아래 결과값은 당연히 10이다.
+        System.out.println("main() : x = " + d.x);
+
+        changeObject(d);
+        System.out.println("main() : x = " + d.x);
+    }
+
+    static void change(int x) { // 기본형 매개변수
+        x = 1000;
+        System.out.println("change() : x = " + x);
+    }
+
+    static void changeObject(Data d) {
+        d.x = 1000;
+    }
+}
